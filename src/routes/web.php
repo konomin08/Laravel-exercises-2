4b4a37eee5e2ea69860fd4e2f6dc1a8ce1dbc8db
konomin <zako606606@gmail.com>
2025-02-21 23:11:55 +0900

    演習3終了

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,24 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', [TodoController::class, 'index']);
+Route::get('/todos/search', [TodoController::class, 'search']);
 Route::post('/todos', [TodoController::class, 'store']);
-Route::get('/todos',[TodoContoroller::class,'store']);
-Route::patch('/todos/update', [TodoController::class, 'update']);
-Route::delete('/todos/delete', [TodoController::class, 'destroy']);
+Route::patch('/todos/{todo_id}', [TodoController::class, 'update']);
+Route::delete('/todos/{todo_id}', [TodoController::class, 'destroy']);
 
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::patch('/categories/{category_id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{category_id}', [CategoryController::class, 'destroy']);
